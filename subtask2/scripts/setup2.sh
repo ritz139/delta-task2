0d1dbc273917677157526a4ba9755097f66bc5fc
ritz139 <63255218+ritz139@users.noreply.github.com>
2020-08-22 01:19:23 +0530

    Delete setup2.sh

--- subtask2/scripts/setup2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-        echo "alias record='/usr/local/scripts/record.sh'" >> /home/ArmyGeneral/.bashrc
-        echo "alias record='/usr/local/scripts/record.sh'" >> /home/NavyMarshal/.bashrc
-        echo "alias record='/usr/local/scripts/record.sh'" >> /home/AirForceChief/.bashrc
-        echo "alias finalattendance='/usr/local/scripts/finalattendance.sh'" >> /home/ChiefCommander/.bashrc
-        echo "alias nearest='/usr/local/scripts/nearest.sh'" >> /home/ChiefCommander/.bashrc
-
